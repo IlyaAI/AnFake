25d313a54091508c5d064a8189fabc195be09a51
ilya-a-ivanov <ilya.a.ivanov@gmail.com>
2015-02-20 18:05:02 +0600

    [*] bug with getting non-last workspace version fixed

--- AnFake.Integration.Tfs2012/ExpandWorkspace.cs
+++ AnFake.Integration.Tfs2012/ExpandWorkspace.cs
@@ -17,6 +17,9 @@ namespace AnFake.Integration.Tfs2012
 		[RequiredArgument]
 		public InArgument<Workspace> Workspace { get; set; }
 
+		[RequiredArgument]
+		public InArgument<string> Version { get; set; }
+
 		public InArgument<UseWorkspaceFrom> WorkspaceSource { get; set; }
 
 		public InArgument<string> WorkspaceFile { get; set; }
@@ -31,6 +34,7 @@ namespace AnFake.Integration.Tfs2012
 		{
 			var buildDetail = BuildDetail.Get(context);
 			var workspace = Workspace.Get(context);
+			var version = Version.Get(context);
 			var workspaceSource = WorkspaceSource.Get(context);
 			var workspaceFile = WorkspaceFile.Get(context);
 
@@ -44,12 +48,22 @@ namespace AnFake.Integration.Tfs2012
 						" Workspace to be expanded should contain only mapping for folder with '{0}' file.",
 						workspaceFile));
 
+			if (String.IsNullOrEmpty(version))
+			{
+				version = !String.IsNullOrEmpty(buildDetail.SourceGetVersion)
+					? buildDetail.SourceGetVersion
+					: "T"; // Latest
+			}
+
 			var wsPath = ServerPathUtils.Combine(workspace.Folders[0].ServerItem, workspaceFile);
 			var localPath = workspace.Folders[0].LocalItem;
 
-			context.TrackBuildMessage(String.Format("Expanding workspace: {0} => {1}", wsPath, localPath));
+			context.TrackBuildMessage(String.Format("Expanding workspace: {0} => {1} @ {2}", wsPath, localPath, version));
 
-			var wsDesc = GetTextContent(buildDetail.BuildServer.TeamProjectCollection, wsPath);
+			var wsDesc = GetTextContent(
+				buildDetail.BuildServer.TeamProjectCollection, 
+				wsPath, 
+				VersionSpec.ParseSingleSpec(version, "."));
 			var mappings = VcsMappings.Parse(wsDesc, workspace.Folders[0].ServerItem, localPath);
 
 			context.TrackBuildMessage(String.Format("{0} // {1}", workspace.Name, workspace.Comment));
@@ -63,11 +77,11 @@ namespace AnFake.Integration.Tfs2012
 			context.TrackBuildMessage("Workspace sucessfully expanded.");
 		}
 
-		private static string GetTextContent(TfsConnection tfs, string tfsPath)
+		private static string GetTextContent(TfsConnection tfs, string tfsPath, VersionSpec version)
 		{
 			var vcs = tfs.GetService<VersionControlServer>();
 
-			var item = vcs.GetItem(tfsPath);
+			var item = vcs.GetItem(tfsPath, version);
 			using (var downstream = item.DownloadFile())
 			{
 				return new StreamReader(downstream).ReadToEnd();
