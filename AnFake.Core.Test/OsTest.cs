caed06a1c67c6f9a495287766ab14b744230d080
IlyaAI <ilya.a.ivanov@gmail.com>
2016-03-10 12:07:53 +0600

    [+] tests fo Os class added [+] some documentation added

---
+++ AnFake.Core.Test/OsTest.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnFake.Core.Test
+{
+	[TestClass]
+	[SuppressMessage("ReSharper", "InconsistentNaming")]
+	public class OsTest
+	{
+		[TestCategory("Unit")]
+		[TestMethod]
+		public void exe_should_add_exe_on_win()
+		{
+			// arrange
+			Os.Type = Os.TypeName.Windows;
+
+			// act & assert
+			Assert.AreEqual("path/to/executable*.exe", Os.exe("path/to/executable*"));
+		}
+
+		[TestCategory("Unit")]
+		[TestMethod]
+		public void dll_should_add_dll_on_win()
+		{
+			// arrange
+			Os.Type = Os.TypeName.Windows;
+
+			// act & assert
+			Assert.AreEqual("path/to/dynamic-lib*.dll", Os.dll("path/to/dynamic-lib*"));
+		}
+
+		[TestCategory("Unit")]
+		[TestMethod]
+		public void bat_should_add_bat_on_win()
+		{
+			// arrange
+			Os.Type = Os.TypeName.Windows;
+
+			// act & assert
+			Assert.AreEqual("path/to/batch*.bat", Os.bat("path/to/batch*"));
+		}
+
+		[TestCategory("Unit")]
+		[TestMethod]
+		public void exe_should_add_nothing_on_linux()
+		{
+			// arrange
+			Os.Type = Os.TypeName.Linux;
+
+			// act & assert
+			Assert.AreEqual("path/to/executable*", Os.exe("path/to/executable*"));
+		}
+
+		[TestCategory("Unit")]
+		[TestMethod]
+		public void dll_should_add_lib_and_so_on_linux()
+		{
+			// arrange
+			Os.Type = Os.TypeName.Linux;
+
+			// act & assert
+			Assert.AreEqual("path/to/libdynamic-lib*.so", Os.dll("path/to/dynamic-lib*"));
+		}
+
+		[TestCategory("Unit")]
+		[TestMethod]
+		public void bat_should_add_sh_on_linux()
+		{
+			// arrange
+			Os.Type = Os.TypeName.Linux;
+
+			// act & assert
+			Assert.AreEqual("path/to/batch*.sh", Os.bat("path/to/batch*"));
+		}
+	}
+}
--- AnFake.Core/Os.cs
+++ AnFake.Core/Os.cs
@@ -15,13 +15,24 @@ namespace AnFake.Core
 			Linux
 		}
 
+		private static TypeName _type = TypeName.Windows;
+
 		public static TypeName Type
 		{
-			get { return TypeName.Windows; }
+			get { return _type; }
+			internal set { _type = value; }
 		}
 
+		/// <summary>
+		///		Returns &lt;pattern> + '.exe' on Windows and &lt;pattern> on Linux.
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		public static string exe(string pattern)
 		{
+			if (String.IsNullOrWhiteSpace(pattern))
+				throw new ArgumentException("Os.exe(pattern): pattern must not be null or empty");
+
 			switch (Type)
 			{
 				case TypeName.Windows:
@@ -33,21 +44,45 @@ namespace AnFake.Core
 			}
 		}
 
+		/// <summary>
+		///		Returns &lt;pattern> + '.dll' on Windows and 'lib' + &lt;pattern> + '.so' on Linux.
+		/// </summary>
+		/// <remarks>
+		///		If pattern contains slash or back slash then 'lib' prefix will be added to file name (just after last slash).
+		/// </remarks>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		public static string dll(string pattern)
 		{
+			if (String.IsNullOrWhiteSpace(pattern))
+				throw new ArgumentException("Os.dll(pattern): pattern must not be null or empty");
+
 			switch (Type)
 			{
 				case TypeName.Windows:
 					return pattern + ".dll";
 				case TypeName.Linux:
-					return "lib" + pattern + ".so";
+				{
+					var lastSlash = pattern.LastIndexOfAny(new[] {'/', '\\'});
+					return lastSlash >= 0
+						? pattern.Substring(0, lastSlash + 1) + "lib" + pattern.Substring(lastSlash + 1) + ".so"
+						: "lib" + pattern + ".so";
+				}					
 				default:
 					throw new NotSupportedException();
 			}
 		}
 
+		/// <summary>
+		///		Returns &lt;pattern> + '.bat' on Windows and &lt;pattern> + '.sh' on Linux.
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		public static string bat(string pattern)
 		{
+			if (String.IsNullOrWhiteSpace(pattern))
+				throw new ArgumentException("Os.bat(pattern): pattern must not be null or empty");
+
 			switch (Type)
 			{
 				case TypeName.Windows:
@@ -59,18 +94,45 @@ namespace AnFake.Core
 			}
 		}
 
+		/// <summary>
+		///		Returns &lt;pattern> on both Windows and Linux.
+		/// </summary>
+		/// <remarks>
+		///		Actually does nothing, intended for consistency only.
+		/// </remarks>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		public static string ifAny(string pattern)
 		{
+			if (String.IsNullOrWhiteSpace(pattern))
+				throw new ArgumentException("Os.ifAny(pattern): pattern must not be null or empty");
+
 			return pattern;
 		}
 
+		/// <summary>
+		///		Returns &lt;pattern> on Windows and nothing on Linux.
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		public static string ifWindows(string pattern)
 		{
+			if (String.IsNullOrWhiteSpace(pattern))
+				throw new ArgumentException("Os.ifWindows(pattern): pattern must not be null or empty");
+
 			return Type == TypeName.Windows ? pattern : FileSet.SkipPattern;
 		}
 
+		/// <summary>
+		///		Returns &lt;pattern> on Linux and nothing on Windows.
+		/// </summary>
+		/// <param name="pattern"></param>
+		/// <returns></returns>
 		public static string ifLinux(string pattern)
 		{
+			if (String.IsNullOrWhiteSpace(pattern))
+				throw new ArgumentException("Os.ifLinux(pattern): pattern must not be null or empty");
+
 			return Type == TypeName.Linux ? pattern : FileSet.SkipPattern;
 		}
 	}
