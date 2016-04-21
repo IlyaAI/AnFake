d7b7980e3182f2b45dc8872ab05dfebbd821838e
IlyaAI <ilya.a.ivanov@gmail.com>
2016-04-21 17:12:42 +0600

    [+] test build script as csproj added

---
+++ build/build.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using AnFake.Api;
+using AnFake.Core;
+using AnFake.Csx;
+
+namespace build
+{
+    public class BuildSpec
+    {
+	    public BuildSpec()
+	    {
+		    Console.WriteLine("I'm Created!");
+	    }
+
+		[Target]
+		[FailIfAnyWarning]
+		public void Probe()
+		{
+			var cur = "".AsPath();
+			Trace.InfoFormat("Hello! I'm here: {0}", cur.Full);
+
+			//Trace.Warn("ERROR");
+		}
+
+		[TargetOnFailure]
+		public void ProbeOnFailure()
+		{
+			Trace.Info("OnFailure");
+		}
+
+		[TargetFinally]
+		public void ProbeFinally()
+		{			
+			Trace.Info("Finally");
+		}
+    }
+}
