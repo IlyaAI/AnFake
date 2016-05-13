50f761684b5811b7a034f41c76ac5d603efea3b5
IlyaAI <ilya.a.ivanov@gmail.com>
2016-05-13 12:54:48 +0600

    [+] Args.Param(FileSystemPath) overload added

--- AnFake.Core/ArgsExtension.cs
+++ AnFake.Core/ArgsExtension.cs
@@ -1,9 +1,18 @@
-﻿using AnFake.Api;
+﻿using System;
+using AnFake.Api;
 
 namespace AnFake.Core
 {
 	public static class ArgsExtension
 	{
+		public static Args Param(this Args args, FileSystemPath path)
+		{
+			if (path == null)
+				throw new ArgumentException("Args.Param(path): path must not be null");
+
+			return args.Param(path.Full);
+		}
+
 		public static Args Option(this Args args, string name, FileSystemPath path)
 		{
 			return path == null 
